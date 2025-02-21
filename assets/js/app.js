28d8bb7fbce2604ceb63435ca08041555b11b9a5
moisesBarsoti <moises.barsot2@gmail.com>
2025-02-21 18:02:14 -0300

    Falta arrumar a responsividade

---
+++ assets/js/app.js
@@ -0,0 +1,39 @@
+const numeroSecreto = Math.floor((Math.random() * numeroMax) + 1);
+let tentativas = 0
+let numerosJaDigitados = [];
+
+// Clique do botão para enviar o número
+const button = document.querySelector('button');
+
+button.addEventListener('click', function () {
+    let digiteUmValor = document.querySelector('input');
+    const digiteUmValorInt = parseInt(digiteUmValor.value);
+    const feedback = document.querySelector('#feedback');
+    const tentativasDisplay = document.querySelector('#tentativas');
+    const numerosDigitados = document.querySelector('#numerosDigitados');
+
+    tentativas++
+    numerosJaDigitados.push(digiteUmValorInt);
+    
+    // Condição
+    if (numeroSecreto === digiteUmValorInt) {
+        console.log(`Você acertou o número secreto! ${numeroSecreto}`);
+        window.location.href = 'voceAcertou.html';
+    } else if (numeroSecreto < digiteUmValorInt) {
+        feedback.textContent = `O número secreto é menor que: ${digiteUmValorInt}`
+        console.log(`O número secreto é menor que: ${digiteUmValorInt}`);
+    } else {
+        feedback.textContent = `O número secreto é maior que: ${digiteUmValorInt}`
+        console.log(`O número secreto é maior que: ${digiteUmValorInt}`);
+    }
+
+    // Palavras do singular para o plural
+
+    let palavraDigitados = tentativas > 1 ? 'Números digitados:' : 'Número digitado:';
+    let palavraTentativaDeAcertos = tentativas > 1 ? `Tentativa de acertos` : `Tentativa de acerto`;
+    let palavraVez = tentativas > 1 ? 'vezes' : 'vez'
+
+    tentativasDisplay.textContent = `${palavraTentativaDeAcertos} ${tentativas} ${palavraVez}`; 
+    numerosDigitados.textContent = `${palavraDigitados} ${numerosJaDigitados.join(', ')}`;
+    digiteUmValor.value = '';
+});
