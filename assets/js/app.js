fe58dfa030887238d24306a3170538f7000ec9f7
moisesBarsoti <moises.barsot2@gmail.com>
2025-02-22 11:58:12 -0300

    Realizando algumas melhorias no código

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,4 @@
+// Gerando número aleatório
 const numeroSecreto = Math.floor((Math.random() * 100) + 1);
 let tentativas = 0
 let numerosJaDigitados = [];
@@ -6,34 +7,33 @@ let numerosJaDigitados = [];
 const button = document.querySelector('button');
 
 button.addEventListener('click', function () {
-    let digiteUmValor = document.querySelector('input');
-    const digiteUmValorInt = parseInt(digiteUmValor.value);
+    let input = document.querySelector('input');
+    const inputUmValorInt = parseInt(input.value);
     const feedback = document.querySelector('#feedback');
     const tentativasDisplay = document.querySelector('#tentativas');
     const numerosDigitados = document.querySelector('#numerosDigitados');
 
     tentativas++
-    numerosJaDigitados.push(digiteUmValorInt);
+    numerosJaDigitados.push(inputUmValorInt);
     
-    // Condição
-    if (numeroSecreto === digiteUmValorInt) {
+    // Condições
+    if (numeroSecreto === inputUmValorInt) {
         console.log(`Você acertou o número secreto! ${numeroSecreto}`);
         window.location.href = 'voceAcertou.html';
-    } else if (numeroSecreto < digiteUmValorInt) {
-        feedback.textContent = `O número secreto é menor que: ${digiteUmValorInt}`
-        console.log(`O número secreto é menor que: ${digiteUmValorInt}`);
+    } else if (numeroSecreto < inputUmValorInt) {
+        feedback.textContent = `O número secreto é menor que: ${inputUmValorInt}`
+        console.log(`O número secreto é menor que: ${inputUmValorInt}`);
     } else {
-        feedback.textContent = `O número secreto é maior que: ${digiteUmValorInt}`
-        console.log(`O número secreto é maior que: ${digiteUmValorInt}`);
+        feedback.textContent = `O número secreto é maior que: ${inputUmValorInt}`
+        console.log(`O número secreto é maior que: ${inputUmValorInt}`);
     }
 
     // Palavras do singular para o plural
-
     let palavraDigitados = tentativas > 1 ? 'Números digitados:' : 'Número digitado:';
     let palavraTentativaDeAcertos = tentativas > 1 ? `Tentativa de acertos` : `Tentativa de acerto`;
     let palavraVez = tentativas > 1 ? 'vezes' : 'vez'
 
     tentativasDisplay.textContent = `${palavraTentativaDeAcertos} ${tentativas} ${palavraVez}`; 
     numerosDigitados.textContent = `${palavraDigitados} ${numerosJaDigitados.join(', ')}`;
-    digiteUmValor.value = '';
+    input.value = '';
 });
